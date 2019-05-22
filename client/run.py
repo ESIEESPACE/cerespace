1ee97224532472df5973cbac4ec87f60b8b9b376
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2019-05-22 11:26:43 +0200

    Update client to connect to Mqtt brocker

--- client/run.py
+++ client/run.py
@@ -19,7 +19,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect("mqtt", 1883, 60)
 
 client.loop_start()
 
