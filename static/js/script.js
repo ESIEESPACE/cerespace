c6218271eb45d2c67cef194d7844d1bbe63dd8c7
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2019-05-25 10:21:19 +0200

    Add MQTTJS

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,22 @@
+var client = mqtt.connect("ws://0.0.0.0:9001");
+
+client.subscribe("farm/farm1");
+
+client.on("message", function (topic, payload) {
+    console.log([topic, payload].join(": "));
+});
+
 $(".btn-control").click(function (event) {
-    console.log(sender)
-    let btn = $(event.target)
-    if(btn.text() == "Forawd");
+    let btn = $(event.target);
+    let cmd = ["go"];
+    let value = $("#distselect").val();
+    switch (btn.attr("id")) {
+        case "fw":
+            cmd.push([value, 0, 0]);
+            break;
+        default:
+            break;
+    }
 });
 
 $(".distcontroller").change(distChange);
@@ -9,7 +24,7 @@ $("#distselect").on('input', distChange);
 
 function distChange(sender) {
     let val = "";
-    if(typeof sender.target != "undefined") val = $(sender.target).val();
+    if (typeof sender.target != "undefined") val = $(sender.target).val();
     else val = sender;
 
     console.log(val);
